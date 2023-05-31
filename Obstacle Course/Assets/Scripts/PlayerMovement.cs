6ff269c17829de339220a9413bab062cc153dace
Luizfmmiranda8 <luizfmmiranda@gmail.com>
2023-05-31 11:59:40 -0300

    Player movement

---
+++ Obstacle Course/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    #region VARIABLES
+    [Header("Movement")]
+    [SerializeField] float moveSpeed = 1f;
+    #endregion
+
+    #region EVENTS
+    void Start() 
+    {
+        
+    }
+
+    void Update() 
+    {
+        MovePlayer();
+    }
+    #endregion
+
+    #region METHODS
+    void MovePlayer()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate(xValue, 0, zValue);
+    }
+    #endregion
+}
